44b31f960abadbc56554947821724e319bc45039
ZySerika <glacierblast@163.com>
2023-09-03 22:44:24 -0400

    Use CommandLineRunner to prerun mq init script

--- bi-backend/src/main/java/com/yupi/springbootinit/bizmq/BiInitMain.java
+++ bi-backend/src/main/java/com/yupi/springbootinit/bizmq/BiInitMain.java
@@ -3,30 +3,27 @@ package com.yupi.springbootinit.bizmq;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 
 @Component
-public class BiInitMain {
+public class BiInitMain implements CommandLineRunner {
 
-    @PostConstruct
-    public void init() {
-        try {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-
-            String EXCHANGE_NAME = BiMqConstant.BI_EXCHANGE_NAME;
+    @Override
+    public void run(String... args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
+            String EXCHANGE_NAME = "bi_exchange";
             channel.exchangeDeclare(EXCHANGE_NAME, "direct");
 
-            String queueName = BiMqConstant.BI_QUEUE_NAME;
+            String queueName = "bi_queue";
             channel.queueDeclare(queueName, true, false, false, null);
-            channel.queueBind(queueName, EXCHANGE_NAME, BiMqConstant.BI_ROUTING_KEY);
-
+            channel.queueBind(queueName, EXCHANGE_NAME, "bi_routing_key");
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 }
